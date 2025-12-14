f00f94c0c8595741a09657388ef8c7625ccdb808
Edem Wilfried AMOUZOU <edemwilfriedamouzou@gmail.com>
2025-12-14 14:42:27 +0000

    Change database password to a secure placeholder
    Updated database password to a secure placeholder.

--- config.php
+++ config.php
@@ -3,7 +3,7 @@ $host = '127.0.0.1';
 $port = 3307;
 $db_name = 'Gestion_Com';
 $username = 'root';
-$password = 'will';
+$password = '****';
 
 try{
     $con = new PDO("mysql:host=$host;port=$port;dbname=$db_name",$username,$password);
